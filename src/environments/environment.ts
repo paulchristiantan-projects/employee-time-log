a23f66a9c17db9820563dee73d71a6b6018f5321
Paul Christian Tan <paulchristian.tan@infor.com>
2026-01-12 22:00:18 +0800

    Edit environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: process.env['NG_APP_FIREBASE_API_KEY'] || '',
+    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || '',
+    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || '',
+    storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || '',
+    messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || '',
+    appId: process.env['NG_APP_FIREBASE_APP_ID'] || ''
+  },
+  admin: {
+    employeeNumber: process.env['NG_APP_ADMIN_EMPLOYEE_NUMBER'] || 'ADMIN001',
+    password: process.env['NG_APP_ADMIN_PASSWORD'] || 'Infor123!'
+  }
+};
